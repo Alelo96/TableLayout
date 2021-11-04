527e668f9778b37a249a0bd8c3a20e6aa5eca1c4
Alex Lopez <alexlopez@inspladelestany.cat>
2021-11-04 10:11:15 +0100

    Initial commit

---
+++ app/src/main/java/cat/dam/alex/tablelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.dam.alex.tablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
